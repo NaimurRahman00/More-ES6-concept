8ce495c2b1f819e6a01e2761c13124d71c9b5fed
Naimur00 <aminaim4u@gmail.com>
2024-02-24 16:16:35 +0600

    Find js added

--- find.js
+++ find.js
@@ -3,4 +3,11 @@ You are given an array say: [33, 50, 79, 78, 90, 101, 30].
 Now return/get all the elements which are divisible by 10 using array.filter() method.
 
 Now do the same task of question 2. But do this using array.find() method. Then compare the output of question 2 & 3.
-*/ 
+*/
+
+const array = [33, 50, 79, 78, 90, 101, 30];
+
+const filter = array.filter(n => n % 10 === 0);
+const find = array.find(n => n % 10 === 0);
+
+console.log('filter: ',filter,'find: ', find);
